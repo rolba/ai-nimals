8d359a489d8d18ed60095c1f76259208e85379ae
Ai-Experiments by Rolba <rolba@users.noreply.github.com>
2019-10-10 08:23:27 +0200

    Cleanup of unused values.

--- ai_nimals_prepare_scrapped.py
+++ ai_nimals_prepare_scrapped.py
@@ -16,7 +16,6 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
            "sofa", "train", "tvmonitor"]
-COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 # load our serialized model from disk
 print("[INFO] loading model...")
