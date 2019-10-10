24f7cd26e67611d94cc8fb57e3acfb8f33813920
Ai-Experiments by Rolba <rolba@users.noreply.github.com>
2019-10-10 09:25:28 +0200

    General refactoring. Code devided into functions.

--- ai_nimals_prepare_scrapped.py
+++ ai_nimals_prepare_scrapped.py
@@ -23,70 +23,84 @@ detectionNet = cv2.dnn.readNetFromCaffe(prototxt, model)
 os.environ["PATH"] += os.pathsep + os.getcwd()
 datasetPath = os.getcwd() + "/Downloads"
 
-for (i, imagePath) in enumerate(os.listdir(datasetPath)):
-    if os.path.exists(join(datasetPath, imagePath, "detected")):
-        shutil.rmtree(join(datasetPath, imagePath, "detected"))
+def padCroppedImage(crop_img):
+    # Let's do some padding! That helps achieving square images for training CNNs
+    height, width, channels = crop_img.shape
 
-    # 	if 	not os.path.exists(join (datasetPath,imagePath,"detected")):
-    os.makedirs(join(datasetPath, imagePath, "detected"))
-    onlyfileslist = [f for f in listdir(join(datasetPath, imagePath)) if isfile(join(datasetPath, imagePath, f))]
+    # Pad left and right side of image with zeros. Or...
+    if height > width:
+        fillpix = int((height - width) / 2)
+        crop_img = cv2.copyMakeBorder(crop_img, 0, 0, fillpix, fillpix, cv2.BORDER_CONSTANT, 0)
 
-    images = np.empty(len(onlyfileslist), dtype=object)
+    # ... pad top and bottom side of image with zeros.
+    if height < width:
+        fillpix = int((width - height) / 2)
+        crop_img = cv2.copyMakeBorder(crop_img, fillpix, fillpix, 0, 0, cv2.BORDER_CONSTANT, 0)
+    return crop_img
 
-    # Now let's loop over all files in range of one class.
-    for n in range(0, len(onlyfileslist)):
-        print(join(datasetPath, imagePath, onlyfileslist[n]))
+def findBirdInFrame(frame):
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (600, 600)), 0.007843, (300, 300), 127.5)
 
-        frame = cv2.imread(join(datasetPath, imagePath, onlyfileslist[n]), cv2.IMREAD_COLOR)
-        if frame is None:
-            print("Can not read image")
-        else:
-            # grab the frame dimensions and convert it to a blob
-            (h, w) = frame.shape[:2]
-            blob = cv2.dnn.blobFromImage(cv2.resize(frame, (600, 600)), 0.007843, (300, 300), 127.5)
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    detectionNet.setInput(blob)
+    detections = detectionNet.forward()
 
-            # pass the blob through the network and obtain the detections and
-            # predictions
-            detectionNet.setInput(blob)
-            detections = detectionNet.forward()
+    # loop over the detections done by MobieNet SSD.
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
+        confidence = detections[0, 0, i, 2]
 
-            # loop over the detections done by MobieNet SSD.
-            for i in np.arange(0, detections.shape[2]):
-                # extract the confidence (i.e., probability) associated with
-                # the prediction
-                confidence = detections[0, 0, i, 2]
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence > confidence_threshold:
+            # extract the index of the class label from the
+            # `detections`, then compute the (x, y)-coordinates of
+            # the bounding box for the object
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            crop_img = frame[startY:startY + (endY - startY), startX:startX + (endX - startX)]
+            break
+    return crop_img
 
-                # filter out weak detections by ensuring the `confidence` is
-                # greater than the minimum confidence
-                if confidence > confidence_threshold:
+def main():
+    for (i, imagePath) in enumerate(os.listdir(datasetPath)):
 
-                    # extract the index of the class label from the
-                    # `detections`, then compute the (x, y)-coordinates of
-                    # the bounding box for the object
-                    idx = int(detections[0, 0, i, 1])
-                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                    (startX, startY, endX, endY) = box.astype("int")
-                    crop_img = frame[startY:startY + (endY - startY), startX:startX + (endX - startX)]
-                    break
+        # Delete all previous detections
+        if os.path.exists(join(datasetPath, imagePath, "detected")):
+            shutil.rmtree(join(datasetPath, imagePath, "detected"))
 
-            # Let's do some padding! That helps achieving square images for training CNNs
-            height, width, channels = crop_img.shape
+        os.makedirs(join(datasetPath, imagePath, "detected"))
+        onlyfileslist = [f for f in listdir(join(datasetPath, imagePath)) if isfile(join(datasetPath, imagePath, f))]
 
-            # Pad left and right side of image with zeros. Or...
-            if height > width:
-                fillpix = int((height - width)/2)
-                crop_img = cv2.copyMakeBorder(crop_img, 0, 0, fillpix, fillpix, cv2.BORDER_CONSTANT, 0)
+        images = np.empty(len(onlyfileslist), dtype=object)
 
-            # ... pad top and bottom side of image with zeros.
-            if height < width:
-                fillpix = int((width - height) / 2)
-                crop_img = cv2.copyMakeBorder(crop_img, fillpix, fillpix, 0, 0, cv2.BORDER_CONSTANT, 0)
+        # Now let's loop over all files in range of one class.
+        for n in range(0, len(onlyfileslist)):
+            print(join(datasetPath, imagePath, onlyfileslist[n]))
 
-            # Make vertical flip of cropped image. Will be need on classes verification script.
-            vertical_flip = cv2.flip(crop_img, 1)
+            frame = cv2.imread(join(datasetPath, imagePath, onlyfileslist[n]), cv2.IMREAD_COLOR)
+            if frame is None:
+                print("Can not read image")
+            else:
+                # Find one(!) bird on loaded image and crop it.
+                croppedImg = findBirdInFrame(frame)
 
-            # Save image
-            cv2.imwrite(join(datasetPath, imagePath, "detected", str(n) + '.png'), vertical_flip)
+                # Pad cropped images with 0 to have square images for future processing
+                paddedCropImg = padCroppedImage(croppedImg)
 
-# do a bit of cleanup
-cv2.destroyAllWindows()
+                # Make vertical flip of cropped image. I am working on verification feature so I will need this anyway
+                paddedCropImg = cv2.flip(paddedCropImg, 1)
+
+                # Save image
+                cv2.imwrite(join(datasetPath, imagePath, "detected", str(n) + '.png'), paddedCropImg)
+
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
