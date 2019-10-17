88e1afcabfbb28361ae8f850727f6db2de7eeb40
Pawel Rolbiecki <pawel.rolbiecki@gmail.com>
2019-10-17 21:54:03 +0200

    Reworking whole duplicated images script

--- ai_nimals_prepare_cropped.py
+++ ai_nimals_prepare_cropped.py
@@ -24,54 +24,65 @@ def getBwLittleImgs(datasetPath):
             os.makedirs(bwDir)
 
         # Iterate over all images in directory
-        for (j, imgPath) in enumerate(os.listdir(imgDir)):
+        for (j, imgName) in enumerate(os.listdir(imgDir)):
 
             # Construct image patch
-            imgPath = join(imgDir, imgPath)
+            imgPath = join(imgDir, imgName)
 
             image = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE)
             if image is not None:
                 resized_image = cv2.resize(image, (32, 32))
                 resized_image = np.array(resized_image)
-                cv2.imwrite(os.path.join(bwDir, str(j)+'.jpg'), resized_image)
+                cv2.imwrite(os.path.join(bwDir, imgName), resized_image)
             else:
                 print(imgPath)
                 os.remove(imgPath)
 
 
-def cleanDuplImgs(datasetPath, rmsErrorThreshold = 5):
+def deleteDupl(imgPath):
+    os.remove(imgPath)
 
-    for (i, classPath) in enumerate(os.listdir(datasetPath)):
-        imgDir = join(datasetPath, classPath, "detected")
-        for (j, imgName) in enumerate(os.listdir(imgDir)):
-            imgPath = join(imgDir, imgName)
-            if os.path.exists(imgPath):
-                image = np.array(cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE))
+def findDelDuplBw(searchedName, bwDir):
+        searchedImg = join(bwDir, searchedName)
 
-                for (k, cmpImgName) in enumerate(os.listdir(imgDir)):
-                    if imgName == cmpImgName:
-                        pass
-                    else:
-                        cmpImgPath = join(imgDir, cmpImgName)
-                        if os.path.exists(cmpImgPath):
-                            cmpImage = np.array(cv2.imread(cmpImgPath, cv2.IMREAD_GRAYSCALE))
-
-                            rms = sqrt(mean_squared_error(image, cmpImage))
-                            cor = np.corrcoef(image.flat, cmpImage.flat)
-
-                            if rms < rmsErrorThreshold:
-                                os.remove(cmpImgPath)
-                                print (imgName, cmpImgName, rms, cor[0, 1])
-                        else:
-                            pass
-            else:
+        for (j, cmpImageName) in enumerate(os.listdir(bwDir)):
+            if cmpImageName == searchedName:
                 pass
+            else:
+                cmpImageBw = join(bwDir, cmpImageName)
+
+                try:
+                    searchedImageBw = np.array(cv2.imread(searchedImg, cv2.IMREAD_GRAYSCALE))
+                    cmpImage = np.array(cv2.imread(cmpImageBw, cv2.IMREAD_GRAYSCALE))
+                    rms = sqrt(mean_squared_error(searchedImageBw, cmpImage))
+                except:
+                    continue
+
+                if rms < 3:
+                    os.remove(cmpImageBw)
+                    print (searchedImg, cmpImageName, rms)
+
+def findDelFinal(detectedDir, bwDir):
+    bwFiles = os.listdir(bwDir)
+    for file in os.listdir(detectedDir):
+        if file not in bwFiles:
+            print (file,  " to be deleted")
+            os.remove(os.path.join(detectedDir, file))
 
 def main():
-    datasetPath = os.getcwd() + "/Downloads"
 
-    # getBwLittleImgs(datasetPath)
-    cleanDuplImgs(datasetPath)
+    datasetPath = os.getcwd() + "/Downloads"
+    getBwLittleImgs(datasetPath)
+
+    for (i, classPath) in enumerate(os.listdir(datasetPath)):
+
+        detectedDir = join(datasetPath, classPath, "detected")
+        bwDir = join(datasetPath, classPath, "bwdir")
+        for (i, detectedImg) in enumerate(os.listdir(detectedDir)):
+            findDelDuplBw(detectedImg, bwDir)
+
+        findDelFinal(detectedDir, bwDir)
+        shutil.rmtree(bwDir)
 
 
 
