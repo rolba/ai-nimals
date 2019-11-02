8bfcd5b290e0acae7694b4d301f9fd3937fe792e
Pawel Rolbiecki <pawel.rolbiecki@gmail.com>
2019-11-02 21:47:49 +0100

    Added Labels' encoder.

--- ai_nimals_h5py_generator.py
+++ ai_nimals_h5py_generator.py
@@ -1,7 +1,7 @@
 import h5py
 import os
 import pandas as pd
-from numpy import single
+from sklearn.preprocessing import LabelEncoder
 
 os.environ["PATH"] += os.pathsep + os.getcwd()
 
@@ -20,20 +20,16 @@ dataSet.append(os.path.join(trainingSetPath, "trainSet.csv"))
 dataSet.append(os.path.join(testingSetPath, "testSet.csv"))
 dataSet.append(os.path.join(validatingPath, "validateSet.csv"))
 
-
-
 def main():
     for singleSetPath in dataSet:
-        dataSetCsv = pd.read_csv(singleSetPath)
-
-        print(dataSetCsv.to)
-        # for p in dataSetCsv:
-            # print(p)
-            # label = p.split(os.path.sep)[-2]
-            # print(label)
-            # label.append(label)
-
-    print (dataSet)
+        setFileName = singleSetPath.split(os.path.sep)[-1]
+        dataSetCsv = pd.read_csv(singleSetPath).values
+        le = LabelEncoder()
+        labels = []
+        for p in dataSetCsv:
+            label = p[0].split(os.path.sep)[-3]
+            labels.append(label)
+        labels = le.fit_transform(labels)
 
 
 if __name__ == "__main__":
