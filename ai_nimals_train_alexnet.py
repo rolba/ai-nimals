b81d7279b6731116f808679f8c0bb86f9f3aeb05
Pawel Rolbiecki <pawel.rolbiecki@gmail.com>
2019-11-21 21:11:45 +0100

    Added generators and training procedures.

--- ai_nimals_train_alexnet.py
+++ ai_nimals_train_alexnet.py
@@ -1,8 +1,4 @@
-from sklearn.feature_extraction.image import extract_patches_2d
-
-from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import ImageDataGenerator
-
 from keras.models import Sequential
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Conv2D
@@ -14,10 +10,12 @@ from keras.layers.core import Dense
 from keras.regularizers import l2
 from keras.optimizers import Adam, SGD
 from keras import backend as K
-
+from DataGenerator import DataGenerator as dg
+import numpy as np
+import json
 import os
-import cv2
 import h5py
+
 
 def buildAlexnet(width, height, depth, classes, reg):
         alexnetmodel = Sequential()
@@ -68,19 +66,6 @@ def buildAlexnet(width, height, depth, classes, reg):
 
         return alexnetmodel
 
-def meanPreprocessor(image, r, g, b):
-    (B, G, R) = cv2.split(image.astype("float32"))
-    R = R - r
-    G = G - g
-    B = B - b
-    return cv2.merge([B, G, R])
-
-def resizePreprocessor(image, h, w, inter=cv2.INTER_AREA):
-    return cv2.resize(image, (h, w), interpolation=inter)
-
-def imgToArray(image, dataFormat = None):
-    return img_to_array(image, data_format=dataFormat)
-
 def augumentator():
     aug = ImageDataGenerator(rotation_range=15, zoom_range=0.1,
                              width_shift_range=0.15, height_shift_range=0.15, shear_range=0.1,
@@ -88,16 +73,16 @@ def augumentator():
     return aug
 
 
-def generator():
-    pass
-
 def main():
     splittedSetPath = os.getcwd() + "/dataset"
     trainingSetPath = os.path.join(splittedSetPath, "trainset", "trainSet.h5py")
     testingSetPath = os.path.join(splittedSetPath, "testset", "testSet.h5py")
     validatingPath = os.path.join(splittedSetPath, "validationset", "validateSet.h5py")
     labelsPath = os.path.join(splittedSetPath, "labels", "labels.json")
+    meansPath = os.path.join(splittedSetPath, "trainset", "train_mean.json")
 
+    means = json.loads(open(meansPath).read())
+    print(means)
     trainDb = h5py.File(trainingSetPath, "r")
     print(trainDb["labels"].shape[0])
     testDb = h5py.File(testingSetPath, "r")
@@ -105,14 +90,28 @@ def main():
     valDb = h5py.File(validatingPath, "r")
     print(valDb["labels"].shape[0])
 
-
     alexNetModel = buildAlexnet(width=227, height=227, depth=3, classes=55, reg=0.0003)
     opt = Adam(lr=1e-3)
     alexNetModel.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
+    trainGenerator = dg(trainDb, batchSize = 128, aug = augumentator(), binarize = True, classesNum=55 )
+    trainGenerator.setimageresizer(width = 227, height = 227)
+    trainGenerator.setmeanpreprocessor(means["R"], means["G"], means["B"])
+
+    validateGenerator = dg(valDb, batchSize = 128, aug = augumentator(), binarize = True, classesNum=55 )
+    validateGenerator.setimageresizer(width = 227, height = 227)
+    validateGenerator.setmeanpreprocessor(means["R"], means["G"], means["B"])
 
 
-
+    # train the network
+    alexNetModel.fit_generator(
+        trainGenerator.generator(),
+        steps_per_epoch=trainGenerator.numImages // 128,
+        validation_data=validateGenerator.generator(),
+        validation_steps=validateGenerator.numImages // 128,
+        epochs=200,
+        max_queue_size=128 * 2,
+        verbose=1)
 
 if __name__ == "__main__":
     main()
